ac501fe346266c975627220e5e4db4838798d49c
Kennet <kennet_rl@hotmail.com>
2026-04-22 23:20:51 -0500

    feat(form): Change state num1 and num2

--- react-project/src/components/form.jsx
+++ react-project/src/components/form.jsx
@@ -6,20 +6,20 @@ export default function FormButton() {
 
   const handleSum = () => {
     const sum = Number(num1) + Number(num2);
-    alert(sum);     
+    alert(sum);
   };
 
   return (
     <>
       <p>Number 1</p>
-      <input type="number" 
-            value={num1} 
-            onChange={(e) => setNum1(e.target.value)} />
+      <input type="number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)} />
       <p>Number 2</p>
 
-      <input type="number" 
-            value={num2} 
-            onChange={(e) => setNum2(e.target.value)} />
+      <input type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)} />
       <button onClick={handleSum}> Sum </button>
     </>
   );
